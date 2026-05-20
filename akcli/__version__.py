2ca8fab7f0b8d5ec0a8fcc3c79687de1bb77ef76
Javi Correa <jcorreag@pm.me>
2026-05-20 14:43:32 +0200

    build: Configure dynamic versioning (#50)
    * chore: Get project metadata dinamically
    
    * chore: Change print version format
    
    Print the current app version using v{version} format. For example:
    `akcli --version` will print akcli v0.1.0
    
    * build: Use poetry-dynamic-versioning
    Configure dynamic versioning using the package: `poetry-dynamic-versioning`. The version will dynamically change depending on the repository tag.

--- akcli/__version__.py
+++ akcli/__version__.py
@@ -4,11 +4,26 @@
 Package metadata.
 """
 
-__title__ = "akcli"
-__url__ = ""
-__description__ = "Command-line interface to make requests to Akamai API."
-__epilog__ = f"See documentation at: [u]{__url__}[/u] for more information."
-__version__ = "v0.1.0"
-__author__ = "Javi C. Guerrero"
-__author_email__ = "jcorreag@pm.me"
-__license__ = "MIT"
+from importlib.metadata import PackageNotFoundError, metadata
+
+try:
+    m = metadata("akcli")
+    __title__ = m["Name"]
+    __version__ = m["Version"]
+    __description__ = m["Summary"]
+    __url__ = m.get_all("Project-Url", "")[1].split(", ")[1]
+    __author__ = m["Author"]
+    __author_email__ = m["Author-email"]
+    __license__ = m["License"]
+
+# Handle cases when package is not installed (i.e running with `python -m akcli.main`)
+except (PackageNotFoundError, KeyError):
+    __title__ = "akcli"
+    __version__ = "0.0.0-dev"
+    __description__ = "Command-line interface to make requests to Akamai API."
+    __url__ = f"https://github.com/jvicg/{__title__}"
+    __author__ = "Javier Correa Guerrero"
+    __author_email__ = "jcorreag@pm.me"
+    __license__ = "MIT"
+
+__epilog__ = f"See documentation at: [u]{__url__}[/u] for more information."  # NOTE: Epilog is not being used atm
